e0a53b960ef8e0b22eb2b3a889d4e3aaad95103e
stubbornnessness <1219721814yxj>
2016-10-12 00:05:55 +0800

    1.2版本开始支持carthage

---
+++ SystemServicesDemo/SystemServices_iOS/SystemServices_iOS.h
@@ -0,0 +1,19 @@
+//
+//  SystemServices_iOS.h
+//  SystemServices_iOS
+//
+//  Created by yuanxiaojun on 2016/10/12.
+//  Copyright © 2016年 Shmoopi LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SystemServices_iOS.
+FOUNDATION_EXPORT double SystemServices_iOSVersionNumber;
+
+//! Project version string for SystemServices_iOS.
+FOUNDATION_EXPORT const unsigned char SystemServices_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SystemServices_iOS/PublicHeader.h>
+
+
