90bc1722588c785e7a6d99eae1349489303a97bd
stubbornnessness <1219721814yxj>
2016-11-14 22:55:52 +0800

    3.0.1

--- SystemServicesDemo/Carthage/Build/iOS/SystemServices_iOS.framework/Headers/SystemServices_iOS.h
+++ SystemServicesDemo/Carthage/Build/iOS/SystemServices_iOS.framework/Headers/SystemServices_iOS.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char SystemServices_iOSVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SystemServices_iOS/PublicHeader.h>
 
 
+#import "SystemServices.h"
